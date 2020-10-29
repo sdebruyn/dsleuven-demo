3494a410890ca48530cc7690b2118d85e5ddda69
Sam Debruyn <sam@debruyn.dev>
2020-10-29 17:42:33 +0100

    add data lake

--- main.tf
+++ main.tf
@@ -24,3 +24,18 @@ module "dl_sp" {
   name    = "dsleuven"
   years   = 1
 }
+
+module "data_lake" {
+  depends_on                      = [azurerm_databricks_workspace.dbks, azurerm_resource_group.rg]
+  source                          = "datarootsio/azure-datalake/module"
+  version                         = "0.5.4"
+  data_lake_name                  = "dsleuvendl"
+  region                          = var.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  service_principal_client_id     = module.dl_sp.client_id
+  service_principal_client_secret = module.dl_sp.client_secret
+  service_principal_object_id     = module.dl_sp.object_id
+  storage_replication             = "LRS"
+  databricks_workspace_name       = azurerm_databricks_workspace.dbks.name
+  provision_synapse               = false
+}
